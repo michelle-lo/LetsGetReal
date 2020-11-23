3358bcd475cdd20256d30604d18d8f0b97e838a1
michelle-lo <mlo20@stuy.edu>
2020-11-23 11:14:34 -0500

    built constructor, completed and tested getValue and getNumerator + getDenominator

--- RationalNumber.java
+++ RationalNumber.java
@@ -10,23 +10,33 @@ public class RationalNumber extends RealNumber
   */
   public RationalNumber(int nume, int deno){
     super(0.0);//this value is ignored!
+    if (deno == 0) {
+      numerator = 0;
+      denominator = 1;
+    } else if (deno < 0) {
+      numerator = nume * -1;
+      denominator = deno;
+    } else {
+      numerator = nume;
+      denominator = deno;
+    }
   }
 
   public double getValue(){
-    return 0.0;
+    return (double) numerator / denominator;
   }
 
   /**
   *@return the numerator
   */
   public int getNumerator(){
-    return 0;
+    return numerator;
   }
   /**
   *@return the denominator
   */
   public int getDenominator(){
-    return 0;
+    return denominator;
   }
   /**
   *@return a new RationalNumber that has the same numerator
--- Tester.java
+++ Tester.java
@@ -15,27 +15,31 @@ public class Tester {
     RealNumber g1 = new RealNumber(-10.0);
     RealNumber h1 = new RealNumber(9.0);
     print(g1.add(h1).getValue(), -1.0);
-    System.out.println("\nmultiply()\n");
+    System.out.println("multiply()\n");
     print(g1.multiply(h1).getValue(), -90.0);
     print(c1.multiply(a1).getValue(), 0.0);
     RealNumber i1 = new RealNumber(1.0);
     print(d1.multiply(i1).getValue(), 10000.01);
-    System.out.println("\ndivide()\n");
+    System.out.println("divide()\n");
     RealNumber j1 = new RealNumber(2.0);
     print(b1.divide(b1).getValue(), 1.0);
     print(i1.divide(j1).getValue(), 0.5);
-    System.out.println("\nsubtract()\n");
+    System.out.println("subtract()\n");
     print(d1.subtract(c1).getValue(), 0.01);
     print(h1.subtract(g1).getValue(), 19.0);
-
-
-
-
-
-
-
-
-
+    System.out.println("\n----RationalNumber----\n");
+    System.out.println("getValue()\n");
+    RationalNumber a2 = new RationalNumber(1, 2);
+    RationalNumber zero = new RationalNumber(1, 0);
+    print(a2.getValue(), 0.5);
+    print(zero.getValue(), 0.0);
+    System.out.println("getNumerator & getDenominator()\n");
+    RationalNumber neg = new RationalNumber(9, -1);
+    print(neg.getNumerator(), -9);
+    print(neg.getDenominator(), -1);
+    print(zero.getNumerator(), 0);
+    print(zero.getDenominator(), 1);
+    
 
   }
 
