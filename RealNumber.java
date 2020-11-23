2884295f29f652fe21c1c72cbdccecf18babbf05
michelle-lo <mlo20@stuy.edu>
2020-11-23 02:33:55 -0500

    add done and tested

--- RealNumber.java
+++ RealNumber.java
@@ -39,7 +39,9 @@ public class RealNumber{
   public RealNumber add(RealNumber other){
      //other can be ANY RealNumber, including a RationalNumber
      //or other subclasses of RealNumber (that aren't written yet)
-     return null;
+     double sumValue = value + other.getValue();
+     RealNumber sum = new RealNumber(sumValue);
+     return sum;
   }
 
   /*
--- Tester.java
+++ Tester.java
@@ -8,6 +8,15 @@ public class Tester {
     RealNumber c1 = new RealNumber(10000);
     RealNumber d1 = new RealNumber(10000.01);
     print(c1.equals(d1), true);
+    System.out.println("\nadd()\n");
+    RealNumber e1 = new RealNumber(c1.add(d1).getValue());
+    RealNumber f1 = new RealNumber(20000.01);
+    print(e1.equals(f1), true);
+    RealNumber g1 = new RealNumber(-10.0);
+    RealNumber h1 = new RealNumber(9.0);
+    print(g1.add(h1).getValue(), -1.0);
+    
+
 
 
   }
